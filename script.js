b6390a0dc4fab4ca314bed4f6aa3404f1c2a8282
tempalfaj <116239001+tempalfaj@users.noreply.github.com>
2022-10-20 22:42:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -125,3 +125,18 @@ const toggleNavbar = () => {
 };
 
 mobile_nav.addEventListener("click", () => toggleNavbar());
+
+
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup").style.display = "block";
+        },
+        1000
+    )
+});
+
+
+document.querySelector("#close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+});
